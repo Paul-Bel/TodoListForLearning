9b9f9621267144b7aadbe7ec408834beb8e7b995
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-18 02:09:09 +0300

    catch error message

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,7 +1,8 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-    status: 'loading' as RequestStatusType
+    status: 'loading' as RequestStatusType,
+    error: null as null | string,
 }
 
 type InitialStateType = typeof initialState
@@ -10,11 +11,17 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
-export type ActionAppSetType = {type: 'APP/SET-STATUS', status: RequestStatusType}
-export const setAppStatusAC = (status: RequestStatusType): ActionAppSetType => ({type: 'APP/SET-STATUS', status})
-// export const setAppStatusAC_I = (): ActionAppSetType => ({type: 'APP/SET-STATUS', status: 'idle'})
+export type ActionAppSetType = ActionAppSetDateType | ActionAppSetErrorType
+export type ActionAppSetDateType = {type: 'APP/SET-STATUS', status: RequestStatusType}
+export type ActionAppSetErrorType = {type: 'APP/SET-ERROR', error: string|null}
+export const setAppStatusAC = (status: RequestStatusType): ActionAppSetDateType => ({type: 'APP/SET-STATUS', status})
+export const setAppErrorAC = (error: string|null): ActionAppSetErrorType => ({type: 'APP/SET-ERROR', error})
