4c874543a777c441bc2ba573df1a943aa44e7fdd
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-04-07 17:21:13 +0300

    refresh

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -42,7 +42,7 @@ function App({demo = false}: PropsType) {
                 <Route path="/" element={<TodolistsList demo={demo}/>}/>
                 <Route path="/login" element={<Login/>}/>
                     <Route path="/404" element={<NotFound/>}/>
-                {/*<Route path="*" element={<Navigate to="/404"/>}/>*/}
+                <Route path="*" element={<Navigate to="/404"/>}/>
                     {/*<TodolistsList demo={demo}/>*/}
             </Routes>
         </Container>
